4e0892b2720b5542c9d0ba120cecefcdd7786665
Bi-Ruei, Chiu <biruei.chiu@gmail.com>
2020-12-09 15:41:41 +0800

    implement code that pulls in our shop data from firestore and stores it into the shop reducer from our shop page. Also included our withSpinner HOC

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,20 +2,31 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {
-  //apiKey: 'AIzaSyAfP214m1V2G0XSpdvvO9exwN1vbuf1njg',
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  //messagingSenderId: '469172431766',
-  //appId: '1:469172431766:web:bd104d1c0ac3bbd9',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65',
-};
+// const config = {
+//   //apiKey: 'AIzaSyAfP214m1V2G0XSpdvvO9exwN1vbuf1njg',
+//   apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
+//   authDomain: 'crwn-db.firebaseapp.com',
+//   databaseURL: 'https://crwn-db.firebaseio.com',
+//   projectId: 'crwn-db',
+//   storageBucket: 'crwn-db.appspot.com',
+//   //messagingSenderId: '469172431766',
+//   //appId: '1:469172431766:web:bd104d1c0ac3bbd9',
+//   messagingSenderId: '850995411664',
+//   appId: '1:850995411664:web:7ddc01d597846f65',
+// };
 
+var config = {
+  apiKey: 'AIzaSyDDXjVECV1c2o0kjeB_lpjdVq_boRjVQKw',
+  authDomain: 'crwn-db-6f52b.firebaseapp.com',
+  projectId: 'crwn-db-6f52b',
+  storageBucket: 'crwn-db-6f52b.appspot.com',
+  messagingSenderId: '556552918358',
+  appId: '1:556552918358:web:79a28bb8624d1bcbb732f7',
+  measurementId: 'G-KSJY6DDXSB',
+};
+// Initialize Firebase
 firebase.initializeApp(config);
+// firebase.analytics();
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
@@ -40,6 +51,39 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   }
 
   return userRef;
+};
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach((obj) => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
 };
 
 export const auth = firebase.auth();
